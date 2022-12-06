16ee89e165cc8897fc3b64efde6661bea23c3caf
Thor <thor@fambagge.dk>
2022-12-06 07:34:25 +0100

    Day 6

---
+++ src/day6/day6_part2.py
@@ -0,0 +1,7 @@
+data = open('input.txt', 'r').read().strip()
+distinct_chars_to_find = 14
+for i in range(len(data)):
+    chars = set(data[i:i + distinct_chars_to_find])
+    if len(chars) == distinct_chars_to_find:
+        print(i + distinct_chars_to_find)
+        break
